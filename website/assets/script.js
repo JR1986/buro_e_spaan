e9347319f58c995a5f32f1921442584c5716edb0
Jelle <jrienstra86@gmail.com>
2018-12-09 15:34:46 +0100

    WIP

--- website/assets/script.js
+++ website/assets/script.js
@@ -1,8 +1,40 @@
 $(document).ready(function() {
-  $("['href=over-ons']").click(function(event) {
+  $("a[href='#over-ons']").click(function(event) {
     $("body, html").animate(
       {
         scrollTop: $("#over-ons").offset().top
+      },
+      1000
+    );
+  });
+  $("a[href='#onze-diensten']").click(function(event) {
+    $("body, html").animate(
+      {
+        scrollTop: $("#onze-diensten").offset().top
+      },
+      1200
+    );
+  });
+  $("a[href='#samenwerking']").click(function(event) {
+    $("body, html").animate(
+      {
+        scrollTop: $("#samenwerking").offset().top
+      },
+      1400
+    );
+  });
+  $("a[href='#contact']").click(function(event) {
+    $("body, html").animate(
+      {
+        scrollTop: $("#contact").offset().top
+      },
+      1600
+    );
+  });
+  $("a[href='#navbar']").click(function(event) {
+    $("body, html").animate(
+      {
+        scrollTop: $("#navbar").offset().top
       },
       1000
     );
