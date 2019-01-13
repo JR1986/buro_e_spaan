3549ab6fbb4d5db837c577ad00929c42cf6111a4
Jelle <jrienstra86@gmail.com>
2019-01-13 14:51:05 +0100

    fix mobile menu

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,13 @@
 $(function() {
-	$('.nav-link, .back-to-top, .button').click(function() {
-		$('html, body').animate(
-			{
-				scrollTop: $($(this).attr('href')).offset().top
-			},
-			900
-		);
-	});
-	$('.menu-button').click(function() {
-		$('.nav-menu').toggleClass('nav-menu-open');
-	});
+  $(".nav-link, .back-to-top, .button").click(function() {
+    $("html, body").animate(
+      {
+        scrollTop: $($(this).attr("href")).offset().top
+      },
+      900
+    );
+  });
+  $(".menu-button").click(function() {
+    $(".nav-menu").toggleClass("nav-menu-open");
+  });
 });
