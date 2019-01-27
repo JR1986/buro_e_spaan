214e01638d5efe65dcd2b08d095e9c120ce8d740
Jelle <jrienstra86@gmail.com>
2019-01-27 11:30:49 +0100

    Try redirect subdomain

---
+++ public/script.min.760938df69fc29b14f84794a77a6f6742768234f82d03e3c893687d8e70372f8.js
@@ -0,0 +1,1 @@
+$(function(){$(".nav-link, .back-to-top, .button").click(function(){$("html, body").animate({scrollTop:$($(this).attr("href")).offset().top},900);});$(".menu-button").click(function(){$(".nav-menu").toggleClass("nav-menu-open");});});
